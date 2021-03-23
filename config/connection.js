dad3e4092be9739459f66bcbb3b1d5c8c99b8536
dave vebber <davevebber@gmail.com>
2021-03-22 18:30:18 -0700

    .env and connection setup for mysql

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,14 @@
 const Sequelize = require('sequelize');
+
+// dotenv package to store sql user and password
+require('dotenv').config();
+
+const sequelize = process.env.JAWSDB_URL
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize(process.env.BD_NAME, process.env.DB_USER,process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+
+    module.exports = sequelize;
