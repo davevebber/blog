c0af7fe007f26670ad83807516883891633bc088
dave vebber <davevebber@gmail.com>
2021-03-24 19:00:54 -0700

    routes complete

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,62 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.get('/', (req, res) => {
+    Comment.findAll({
+      attributes: [
+          'id',
+          'comment_text',
+          'user_id',
+          'post_id'
+      ],
+      order: [['created_at', 'DESC']]
+    })
+      .then(dbCommentData => res.json(dbCommentData))
+
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+  router.post('/', withAuth, (req, res) => {
+    // expects => {comment_text: "This is the comment", user_id: 1, post_id: 2}
+     // check the session
+  // console.log("Comment post");
+  // console.log(req.session);
+  if (req.session) {
+    Comment.create({
+      comment_text: req.body.comment_text,
+      post_id: req.body.post_id,
+      user_id: req.session.user_id
+    })
+      .then(dbCommentData =>  res.json(dbCommentData))
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  }
+  });
+  //add put route
+
+  //delete comment
+  router.delete('/:id', (req, res) => {
+    Comment.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbCommentData => {
+        if (!dbCommentData) {
+          res.status(404).json({ message: 'No comment found with this id!' });
+          return;
+        }
+        res.json(dbCommentData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+module.exports = router;
---
+++ controllers/home-routes.js
@@ -0,0 +1,109 @@
+// contains all of the user-facing routes such as homepage and login page
+const router = require('express').Router();
+const session = require('express-session');
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+  Post.findAll({
+    attributes: [
+      'id',
+      'title',
+      'post_body',
+      'created_at'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id','user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      console.log("RENDER PAGE");
+      // pass a single post object into the homepage template
+      console.log(req);
+      console.log(req.session);
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      const response_body = { 
+        posts,
+        loggedIn: req.session.loggedIn,
+      }
+      res.render('homepage', { 
+        posts,
+        loggedIn: req.session.loggedIn,
+        username: req.session.username
+      });
+    })
+    .catch(err => {
+      console.log("REACHING ERROR IN HOME ROUTE");
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
+
+router.get('/post/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'title',
+      'post_body',
+      'created_at'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+
+      // serialize the data
+      const post = dbPostData.get({ plain: true });
+
+      // pass data to template
+      res.render('single-post', { 
+        post,
+      loggedIn: req.session.loggedIn,
+      username: req.session.username
+     });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
